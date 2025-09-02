5a7740edf3859c14197cc3b7de4332769696bd66
mahmoud radwan <medodiscordban@gmail.com>
2025-09-02 09:46:06 +0300

    model

---
+++ src/model_training.py
@@ -0,0 +1,34 @@
+import os
+
+from tensorflow.keras.datasets.mnist import load_data
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+
+BASE_DIR = os.path.dirname(__file__)
+ROOT = os.path.dirname(BASE_DIR)
+MODELS_PATH = os.path.join(ROOT, 'models')
+
+(x_train, y_train), (x_test, y_test) = load_data()
+
+x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(2, 2))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test))
+
+model.save(os.path.join(MODELS_PATH, 'digit_recognition.keras'))
+
