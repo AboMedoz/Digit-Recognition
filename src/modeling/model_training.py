9635433b9f8b11e4459aed920dfa5c553eb4b1b2
mahmoud radwan <medodiscordban@gmail.com>
2026-07-27 10:20:19 +0300

    model_training

--- src/modeling/model_training.py
+++ src/modeling/model_training.py
@@ -3,19 +3,14 @@ import os
 from tensorflow.keras.datasets.mnist import load_data
 from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
 
-BASE_DIR = os.path.dirname(__file__)
-ROOT = os.path.dirname(BASE_DIR)
+ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 MODELS_PATH = os.path.join(ROOT, 'models')
 
 (x_train, y_train), (x_test, y_test) = load_data()
 
 x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
-
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
@@ -26,7 +21,7 @@ model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test))
 
